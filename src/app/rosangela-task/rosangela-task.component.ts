1c165d73466d392d978932629a5806bc2ba3b9dd
RosangelaSSBorges <rosangelassb@hotmail.com>
2021-12-01 15:23:58 -0300

    Initial commit

---
+++ src/app/rosangela-task/rosangela-task.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rosangela-task',
+  templateUrl: './rosangela-task.component.html',
+  styleUrls: ['./rosangela-task.component.css']
+})
+export class RosangelaTaskComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
